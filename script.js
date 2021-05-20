ee984bc607c189c50997d4bea6f66ce486159c21
Ankush Chavhan <77318013+AnkushChavhan7596@users.noreply.github.com>
2021-05-20 10:54:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function res() {
+  document.getElementById("screen").value = "";
+}
+
+function Show(input) {
+  document.getElementById("screen").value += input; // sreen= (Previous screen value ) + (new input)
+}
+
+function cal() {
+  var output = eval(document.getElementById("screen").value);
+  document.getElementById("screen").value = output;
+}
+
+function backspace() {
+  var k = document.getElementById("screen").value;
+  document.getElementById("screen").value = k.substring(0, k.length - 1);
+}
